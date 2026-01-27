0ae04144d549cfa6ef68978655bb80a1309bdb38
Kyrollos Emad <kyrollos.emad05@gmail.com>
2026-01-27 19:26:49 +0100

    Update api endpoint in flutter app

--- mobile/lib/core/config/api_config.dart
+++ mobile/lib/core/config/api_config.dart
@@ -1,5 +1,5 @@
 class ApiConfig {
-  static const String baseUrl = 'http://wadeea.strefqa.com:3000';
+  static const String baseUrl = 'https://api.wadeea.strefqa.com';
   static const String googleServerClientId =
       '148173582432-vm6brkl29h086k10kamd078tajuvubpi.apps.googleusercontent.com';
 }
